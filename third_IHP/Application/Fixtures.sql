aa50acb1b01e418658b76e15c9e3f6e252dc864a
peter <peter@Susans-MacBook-Air-2.local>
2021-04-01 20:36:14 -0700

    this is where I got stuck trying to make the blog from scratch

---
+++ third_IHP/Application/Fixtures.sql
@@ -0,0 +1,31 @@
+
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+
+SET SESSION AUTHORIZATION DEFAULT;
+
+ALTER TABLE public.posts DISABLE TRIGGER ALL;
+
+INSERT INTO public.posts (id, title, body) VALUES ('75bc611d-1f0b-407d-9ce4-473c57964352', '!!Heya Boii', 'miya bodee');
+
+
+ALTER TABLE public.posts ENABLE TRIGGER ALL;
+
+
+ALTER TABLE public.comments DISABLE TRIGGER ALL;
+
+
+
+ALTER TABLE public.comments ENABLE TRIGGER ALL;
+
+
