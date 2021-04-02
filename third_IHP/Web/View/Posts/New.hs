c7d02c1fa10eaf1ac299d52a6733ef116c1402ef
peter <peter@Susans-MacBook-Air-2.local>
2021-04-01 21:10:34 -0700

    a question regarding how the form directs to CreatePostAction

--- third_IHP/Web/View/Posts/New.hs
+++ third_IHP/Web/View/Posts/New.hs
@@ -21,3 +21,6 @@ renderForm post = formFor post [hsx|
     {(textField #body)}
     {submitButton}
 |]
+
+-- question : I know that the after submitting this form for a post it'll direct to CreatePostAction, but how is this? For example I don't see a href that mentions this data 
+-- like <a href={pathTo NewPostAction} >  ... index.hs that directs to this route... so is this because of the autoRouter?
